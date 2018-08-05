5337246e06bb02ae9678accc05efd792ca88febb
Your Name <thada_rock@hotmail.com>
2018-08-05 22:29:09 +0700

    work02

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+
+<template>
+<v-parallax src="/pic/back.jpg"  >
+  <div>
+    <br>
+      <router-link :to="{ path: '/student'}" replace><center>
+      <div class="text-xs-center">
+        <v-btn round color="black" dark>Welcome</v-btn>
+      </div>
+    </center>
+    </router-link>
+  </div>
+
+</v-parallax>
+
+</template>
+
+<script>
+export default {
+  layout : "default",
+  data() {
+    return {
+      id: 1,
+    }
+  },
+}
+</script>
---
+++ pages/profile.vue
@@ -0,0 +1,8 @@
+<template>
+<v-app>
+    <center><img src="/pic/2.jpg" width="443" height="599"></center>
+   
+      
+</v-app>
+</template>
+
